905bdab7472f28fb0a77b16f37a9db619535e4a1
joshualemli <jlemli@uwyo.edu>
2018-06-15 16:26:44 -0600

    weapons can draw firing status on screen

--- terraform.js
+++ terraform.js
@@ -9,7 +9,7 @@
 (function(){ // START
 /////////////// START
 
-console.log("terraform v0.3.0 by Joshua A. Lemli 2018")
+console.log("terraform v0.3.1 by Joshua A. Lemli 2018")
 
 // 2d vectors only
 const normalizeVector = (v,m) => {
@@ -20,19 +20,50 @@ const normalizeVector = (v,m) => {
     }
     else return v
 }
+// id generating functions
+const _transmogrify = (digitInt) => {
+    if (digitInt < 10) return digitInt.toString()
+    if (digitInt < 36) return String.fromCharCode(digitInt + 87) // - 10 + 97
+    return String.fromCharCode(digitInt + 29) // - 36 + 65
+}
+const _base10Int_to_base62String = (n) => {
+    var exp = 0
+    var strChars = []
+    do {
+        var div = Math.pow(62,exp)
+        var curVal = n % (div*62)
+        var digitInt = curVal / div
+        strChars.push( _transmogrify(digitInt) )
+        n -= curVal
+        exp += 1
+    } while (n > 0)
+    var str = ""
+    for (var i = strChars.length; i--;) str += strChars[i]
+    return str
+}
 
 /* - - - - STATE - - - - */
 
 var State = {
     gameLoopIteration: 0,
     gamePaused: false,
+    menuActive: false,
+    playerPanelActive: true,
     entityIdSeed: 0,
-    itemIdSeed: 0
+    itemIdSeed: 0,
+    spriteIdSeed: 0
 }
 
-const toggleMenu = () => {
+const togglePlayerPanel = (forceShow = null) => {
+    let e = document.getElementById("playerInfo")
+    State.playerPanelActive = forceShow !== null ? forceShow : !State.playerPanelActive
+    e.style.display = State.playerPanelActive ? "flex" : "none"
+}
+
+const toggleMenu = (forceShow = null) => {
     let e = document.getElementById("menu")
-    e.style.display = e.style.display === "flex" ? "none" : "flex"
+    State.menuActive = forceShow !== null ? forceShow : !State.menuActive
+    e.style.display = State.menuActive ? "flex" : "none"
 }
 
 /* - - - - USER INPUT HANDLER - - - - */
@@ -69,6 +100,23 @@ const UserInput = (function(){
 })()
 
 
+/* - - - - PLAYER PANEL - - - - */
+
+const PlayerPanel = (function(){
+
+    var itemElements = new Map()
+    const addItemElement = (item) => {
+        let container = document.createElement("div")
+
+    }
+
+    return {
+        addItem: (item) => {
+
+        }
+    }
+})()
+
 
 /* - - - - CANVAS SUB-API - - - - */
 
@@ -85,13 +133,14 @@ const CanvasArtist = (function(){
         resizeCanvas()
         window.addEventListener("resize",resizeCanvas)
     }
+    const worldTransform = useWorld => {
+        if (useWorld) context.setTransform(magnification,0,0,-magnification, xDimPx/2 - xPos*magnification, yPos*magnification + yDimPx/2)
+        else context.setTransform(1,0,0,1,0,0)
+    }
     const reset = () => {
         context.setTransform(1,0,0,1,0,0)
         context.clearRect(0,0,xDimPx,yDimPx)
-        context.setTransform(magnification,0,0,-magnification,
-            xDimPx/2 - xPos*magnification,
-            yPos*magnification + yDimPx/2
-        )
+        worldTransform(true)
     }
     const fillRect = (x,y,X,Y,fillStyle) => {
         context.fillStyle = fillStyle
@@ -108,6 +157,14 @@ const CanvasArtist = (function(){
         context.arc(x,y,r,0,Math.PI * 2,true)
         context.fill()
     }
+    const strokeLine = (x0,y0,x1,y1,thickness,strokeStyle) => {
+        context.strokeStyle = strokeStyle
+        context.lineWidth = thickness
+        context.beginPath()
+        context.moveTo(x0,y0)
+        context.lineTo(x1,y1)
+        context.stroke()
+    }
     const drawImage = (image,x,y,r) => {
         let L = r*2
         // context.drawImage(image, x-r, y-r, L, L)
@@ -117,14 +174,24 @@ const CanvasArtist = (function(){
     return {
         init:init,
         reset:reset,
+        worldTransform:worldTransform,
         panX: (dx) => xPos += 5 * dx,
         panY: (dy) => yPos += 5 * dy,
         zoom: (multiplier) => magnification *= multiplier,
-        currentViewInfo: () => new Object({xDim:xDimPx/magnification,yDim:yDimPx/magnification,m:magnification,xPos:xPos,yPos:yPos}),
+        currentViewInfo: () => new Object({
+            xDim: xDimPx/magnification,
+            yDim: yDimPx/magnification,
+            m: magnification,
+            xPos: xPos,
+            yPos: yPos,
+            xDimPx: xDimPx,
+            yDimPx: yDimPx
+        }),
         fillRectangle:fillRect,
         outlineRectangle:strokeRect,
         fillCircle:fillCircle,
-        drawImage:drawImage
+        drawImage:drawImage,
+        line:strokeLine
     }
 })()
 
@@ -201,10 +268,113 @@ const SpatialHash = (function(){
     }
 })()
 
+const SpriteFactory = (function(){
+    function ShatterBeam(x0,y0,x1,y1,color) {
+        this.x0 = x0
+        this.y0 = y0
+        this.x1 = x1
+        this.y1 = y1
+        this.color = color
+    }
+    ShatterBeam.prototype.draw = function() {
+        let i = Math.floor(Math.random()*4)
+        for (i; i--;) CanvasArtist.line(
+            this.x0,
+            this.y0,
+            this.x1-4+Math.random()*8,
+            this.y1-4+Math.random()*8,
+            1,
+            `rgba(${ this.color.join(",") })`
+        )
+        Sprites.delete(this.id)
+    }
+    return {
+        spawn: (type,props) => {
+            let sprite = null
+            switch(type.toLowerCase()) {
+                case "shatterbeam":
+                    sprite = new ShatterBeam(props.x0,props.y0,props.x1,props.y1,props.color)
+                    break
+            }
+            State.spriteIdSeed += 1
+            sprite.id = _base10Int_to_base62String(State.spriteIdSeed)
+            Sprites.set(sprite.id,sprite)
+            return sprite
+        }
+    }
+})()
+
 const ItemFactory = (function(){
 
-    function ShatterRay() {
+    // * * * weapon * * *
+    //  prop:owner
+    //  func:fire
+    //  func:step
+    //  func:stats - allows update of modifiable stats for GUI
+    //  func:report - allows update of firing status (temperature,ammo,charge, etc)
 
+    function ShatterRay() {
+        this.range = 100
+        this.cooldownRate = 0.5
+        this.heatupRate = 1
+        this.t = 0 // "temperature"
+        this.tSafe = 55
+        this.tMax = 90
+        this.firing = false
+        this.owner = null
+    }
+    ShatterRay.prototype.report = function() {
+        let tPerc = this.t/this.tMax
+        let values = []
+        let tSafePerc = this.tSafe/this.tMax
+        if (tPerc < tSafePerc) {
+            if (tPerc == 0) values.push(["#00FF00",0,tSafePerc])
+            else {
+                values.push(["#0000FF",0,tPerc])
+                values.push(["#00FF00",tPerc,tSafePerc])
+            }
+            values.push(["#FFFF00",tSafePerc,1])
+        }
+        else {
+            values.push(["#0000FF",0,tPerc])
+            values.push(["#FF0000",tPerc,1])
+        }
+        return values
+    }
+    ShatterRay.prototype.stats = function(update) {
+        return [
+            {name:"Range",key:"range",value:this.range},
+            {name:"Heat Dissipation",key:"cooldownRate",value:this.range},
+            {name:"Heat Generation",key:"heatupRate",value:this.range},
+            {name:"Safe Ignition Temperature",key:"tSafe",value:this.range},
+            {name:"Max Sustained Firing Temperature",key:"tMax",value:this.range},
+        ]
+    }
+    ShatterRay.prototype.fire = function(x,y) {
+        if ((this.firing && this.t < this.tMax) || this.t < this.tSafe) {
+            this.firing = true
+            this.t += this.heatupRate
+            let beamProps = {
+                x0:this.owner.x, y0:this.owner.y,
+                x1:x, y1:y,
+                color: [255,0,0,1]
+            }
+            let dD = [beamProps.x1-beamProps.x0, beamProps.y1-beamProps.y0]
+            dD = normalizeVector(dD,this.range)
+            beamProps.x1 = beamProps.x0 + dD[0]
+            beamProps.y1 = beamProps.y0 + dD[1]
+            SpriteFactory.spawn("ShatterBeam",beamProps)
+        }
+        else this.firing = false
+    }
+    ShatterRay.prototype.step = function() {
+        if (!this.firing) {
+            if (this.t > 0) {
+                this.t -= this.cooldownRate
+                if (this.t < 0) this.t = 0
+            }
+        }
+        else if (!UserInput.keystate("mouse1")) this.firing = false
     }
     ShatterRay.prototype.category = "weapon"
     
@@ -214,7 +384,15 @@ const ItemFactory = (function(){
 
     return {
         spawn: (type) => {
-
+            let item = null
+            switch(type.toLowerCase()) {
+                case "shatterray":
+                    item = new ShatterRay()
+                    break
+            }
+            State.itemIdSeed += 1
+            item.id = _base10Int_to_base62String(State.itemIdSeed)
+            return item
         }
     }
 })()
@@ -223,34 +401,16 @@ const Dungeon = (function(){
 
     var Mortuary = {}
 
-    // -- id generator
-    const _transmogrify = (digitInt) => {
-        if (digitInt < 10) return digitInt.toString()
-        if (digitInt < 36) return String.fromCharCode(digitInt + 87) // - 10 + 97
-        return String.fromCharCode(digitInt + 29) // - 36 + 65
-    }
-    const _base10Int_to_base62String = (n) => {
-        var exp = 0
-        var strChars = []
-        do {
-            var div = Math.pow(62,exp)
-            var curVal = n % (div*62)
-            var digitInt = curVal / div
-            strChars.push( _transmogrify(digitInt) )
-            n -= curVal
-            exp += 1
-        } while (n > 0)
-        var str = ""
-        for (var i = strChars.length; i--;) str += strChars[i]
-        return str
-    }
 
     // -- abilities: traits (properties) and behaviors (methods)
 
     function enable_inventory(maxItems) {
         if (this.prototype) {
             this.prototype.pickupItem = function(item) {
-                if (this.items.size < this.maxItems) this.items.set(item.id,item)
+                if (this.items.size < this.maxItems) {
+                    this.items.set(item.id,item)
+                    item.owner = this
+                }
             }
             this.prototype.dropItems = function(itemsArr) {
 
@@ -336,14 +496,17 @@ const Dungeon = (function(){
     Object.assign(Player.prototype,Entity.prototype)
     enable_inventory.call(Player)
     Player.prototype.step = function() {
-        this.userInput()
         this.move()
+        this.userInput()
+        this.items.forEach( item => item.step() )
     }
     Player.prototype.move = function() {
         this.x += this.dx
         this.y += this.dy
     }
     Player.prototype.userInput = function() {
+        let mouse = UserInput.mouse()
+        // - - - MOVEMENT - - -
         // braking
         if (UserInput.keystate("b")) {
             if (this.dx*this.dx + this.dy*this.dy < this.brakingForce) this.dx = this.dy = 0
@@ -357,9 +520,7 @@ const Dungeon = (function(){
             let accel = [0,0]
             // mouse
             if (UserInput.keystate("mouse3")) {
-                let player = Entities.get(State.playerId)
-                let mouse = UserInput.mouse()
-                accel = [mouse.x - player.x, mouse.y - player.y]
+                accel = [mouse.x - this.x, mouse.y - this.y]
             }
             // keys
             else {
@@ -380,11 +541,11 @@ const Dungeon = (function(){
             this.dx += accel[0]
             this.dy += accel[1]
         }
-        // user input - actions
+        // - - - ITEM USE - - -
         if (UserInput.keystate("mouse1")) {
-            switch(this.activity) {
-                
-            }
+            this.items.forEach( item => {
+                if (item.category === "weapon") item.fire(mouse.x,mouse.y)
+            })
         }
     }
     Player.prototype._imageBitmapPath = "https://raw.githubusercontent.com/joshualemli/terraform/master/images/SpaceMite.png"
@@ -440,21 +601,34 @@ const play = (performanceTime) => {
     if (UserInput.keystate("ArrowRight")) CanvasArtist.panX(1)
     else if (UserInput.keystate("ArrowLeft")) CanvasArtist.panX(-1)
 
-    // world behavior
+    // step
     if (!State.gamePaused) {
         State.gameLoopIteration += 1
         Entities.forEach( entity => entity.step() )
     }        
 
-    // canvas
+    // draw
     CanvasArtist.reset()
-    Entities.forEach( entity => {
-        if (entity.image) CanvasArtist.drawImage(entity.image,entity.x,entity.y,entity.r)
-        else if (entity.lineSprites) CanvasArtist.drawLineSprite(entity.lineSprites)
-        else console.log("ERROR : could not draw entity",entity)
-     } )
+    let viewInfo = CanvasArtist.currentViewInfo()
+    // TO DO: implement spatial culling
+    Entities.forEach( entity => CanvasArtist.drawImage(entity.image,entity.x,entity.y,entity.r) )
+    Sprites.forEach( sprite => sprite.draw() )
+    CanvasArtist.worldTransform(false)
+    Entities.get(State.playerId).items.forEach( (item,index) => {
+        item.report().forEach( (colorBreaksArr) => {
+            // if (Math.random()>0.95) console.log(viewInfo)
+            CanvasArtist.fillRectangle(
+                colorBreaksArr[1] * viewInfo.xDimPx / 10,
+                viewInfo.yDimPx - 4 * index,
+                (colorBreaksArr[2]-colorBreaksArr[1]) * viewInfo.xDimPx / 10,
+                3,
+                colorBreaksArr[0]
+            )
+        })
+    } )
 
     // loop
+    if (UserInput.keystate("t")) console.log(performance.now() - performanceTime)
     window.requestAnimationFrame(play)
 }
 
@@ -480,7 +654,11 @@ const init = () => {
         // *** spawn player ***
         var player = Dungeon.spawn("player")
         State.playerId = player.id
+        player.pickupItem( ItemFactory.spawn("ShatterRay") )
+        // player.pickupItem( ItemFactory.spawn("ShatterRay") )
+        console.log(player)
 
+        
         var foo = Dungeon.spawn("spaceMite",200,200)
         var foo = Dungeon.spawn("spaceMite",-200,200)
         var foo = Dungeon.spawn("spaceMite",200,-200)
